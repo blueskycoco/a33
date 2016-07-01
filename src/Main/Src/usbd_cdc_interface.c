1f93d42ffd664e699e49905ae292db32a8349a1c
U-lf-PC\lf <lf@lf-PC.(none)>
2016-07-01 16:24:21 +0800

    Init version for STM32F302R8T6 , gcc src/usb-pc.c -o usb-pc -lusb

---
+++ src/Main/Src/usbd_cdc_interface.c
@@ -0,0 +1,129 @@
+/**
+  ******************************************************************************
+  * @file    USB_Device/CDC_Standalone/Src/usbd_cdc_interface.c
+  * @author  MCD Application Team
+  * @version V1.3.0
+  * @date    3-July-2015
+  * @brief   Source file for USBD CDC interface
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT(c) 2015 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/** @addtogroup STM32_USB_OTG_DEVICE_LIBRARY
+  * @{
+  */
+
+/** @defgroup USBD_CDC 
+  * @brief usbd core module
+  * @{
+  */ 
+extern void RTC_TimeShow(uint8_t* showtime);
+extern void Error_Handler(void);
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+#define APP_RX_DATA_SIZE  2048
+#define APP_TX_DATA_SIZE  2048
+uint8_t UserRxBuffer[APP_RX_DATA_SIZE];/* Received Data over USB are stored in this buffer */
+uint8_t UserTxBuffer[APP_TX_DATA_SIZE];/* Received Data over UART (CDC interface) are stored in this buffer */
+uint32_t BuffLength;
+uint32_t UserTxBufPtrIn = 0;/* Increment this pointer or roll it back to
+                               start address when data are received over USART */
+uint32_t UserTxBufPtrOut = 0; /* Increment this pointer or roll it back to
+                                 start address when data are sent over USB */
+/* USB handler declaration */
+extern USBD_HandleTypeDef  USBD_Device;
+
+/* Private function prototypes -----------------------------------------------*/
+static int8_t CDC_Itf_Init     (void);
+static int8_t CDC_Itf_DeInit   (void);
+static int8_t CDC_Itf_Control  (uint8_t cmd, uint8_t* pbuf, uint16_t length);
+static int8_t CDC_Itf_Receive  (uint8_t* pbuf, uint32_t *Len);
+
+
+USBD_CDC_ItfTypeDef USBD_CDC_fops = 
+{
+  CDC_Itf_Init,
+  CDC_Itf_DeInit,
+  CDC_Itf_Control,
+  CDC_Itf_Receive
+};
+
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  CDC_Itf_Init
+  *         Initializes the CDC media low layer
+  * @param  None
+  * @retval Result of the opeartion: USBD_OK if all operations are OK else USBD_FAIL
+  */
+static int8_t CDC_Itf_Init(void)
+{
+  USBD_CDC_SetTxBuffer(&USBD_Device, UserTxBuffer, 0);
+  USBD_CDC_SetRxBuffer(&USBD_Device, UserRxBuffer);
+  
+  return (USBD_OK);
+}
+
+/**
+  * @brief  CDC_Itf_DeInit
+  *         DeInitializes the CDC media low layer
+  * @param  None
+  * @retval Result of the opeartion: USBD_OK if all operations are OK else USBD_FAIL
+  */
+static int8_t CDC_Itf_DeInit(void)
+{
+  return (USBD_OK);
+}
+
+/**
+  * @brief  CDC_Itf_Control
+  *         Manage the CDC class requests
+  * @param  Cmd: Command code            
+  * @param  Buf: Buffer containing command data (request parameters)
+  * @param  Len: Number of data to be sent (in bytes)
+  * @retval Result of the opeartion: USBD_OK if all operations are OK else USBD_FAIL
+  */
+static int8_t CDC_Itf_Control (uint8_t cmd, uint8_t* pbuf, uint16_t length)
+{ 
+  return (USBD_OK);
+}
+/**
+  * @brief  CDC_Itf_DataRx
+  *         Data received over USB OUT endpoint are sent over CDC interface 
+  *         through this function.
+  * @param  Buf: Buffer of data to be transmitted
+  * @param  Len: Number of data received (in bytes)
+  * @retval Result of the opeartion: USBD_OK if all operations are OK else USBD_FAIL
+  */
+static int8_t CDC_Itf_Receive(uint8_t* Buf, uint32_t *Len)
+{
+	int i=0;
+	uint8_t RTC_DateTime[10] ={0};
+  //HAL_UART_Transmit_DMA(&UartHandle, Buf, *Len);
+  for(i=0;i<*Len;i++)
+  	printf("%c",Buf[i]);
+  USBD_CDC_ReceivePacket(&USBD_Device);
+  //RTC_TimeShow(RTC_DateTime);
+  printf("\nRTC==>%s\n",RTC_DateTime);
+  return (USBD_OK);
+}
+
