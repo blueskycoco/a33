8e9697c3c0e3e804f873a7570655716b4aa49baa
U-F-7F8724A5CE8D4\bbstr <bbstr@f-7f8724a5ce8d4.(none)>
2016-07-15 09:08:39 +0800

    update make-disco.sh

--- make-disco.sh
+++ make-disco.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 export RTT_EXEC_PATH="d:/Program Files/CodeSourcery/Sourcery_CodeBench_Lite_for_ARM_EABI/bin"
 make
-openocd -f openocd.cfg -c "flash_image"
-#openocd -f openocd.cfg -c "flash_param"
+st-link_cli -c SWD UR LPM -p gcc/net.bin 0x08000000 -Rst -Run
